541fe260bcb29f1d9605d781598bc63ad95ad950
Kirby Johnston <info@spitemonster.com>
2018-08-30 16:30:19 -0400

    drag and drop upload added

--- views/home.vue
+++ views/home.vue
@@ -2,7 +2,7 @@
   <div class="body--wrapper">
     <h1 class="logo">MMS</h1>
     <div class="form--wrapper">
-      <form action="/upload" name="upload" method="post" enctype="multipart/form-data" id="fileUpload">
+      <form action="/upload" name="upload" method="post" multiple enctype="multipart/form-data" id="fileUpload">
         <div class="filename">
           <label for="output-name">Output File Name</label>
           <input type="text" name="outPutName" value="" id="output-name"/>
